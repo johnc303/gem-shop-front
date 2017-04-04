6e4f79e108173e67e20778f021dff1f544f66584
Dicky Illing <kidoblivious@gmail.com>
2017-04-04 21:18:42 +0000

    bad js function tableHeaderAnchors added. Needs attention.

--- _js/global.js
+++ _js/global.js
@@ -1,2 +1,24 @@
 function init() {
 };
+var navbarLinks =
+
+    [ "./index.htm",
+      "./catalogue.htm",
+      "./links.htm",
+      "./about.htm",
+      "./contact.htm" ];
+
+/*
+  tableHeaderAnchors( headers )
+  Takes an array of strings and outputs them as a table header row  
+
+*/
+function tableHeaderAnchors( headers ) {
+
+    var result = "<tr> ";
+    for(i=0;i<headers.length; i++) {
+	result += '<th><a href="' + headers[i] + ">" + headers[i].slice(-4).  + "</a></th>";
+    }
+    result += "</tr>";
+    return result;
+}
