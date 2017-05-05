3cd0e1c1768b2841d9fdc916266e334f8d460dc8
dicky <kidoblivious@gmail.com>
2017-05-05 16:23:16 +0100

    Small changes to make carousel template work.

--- learning/lesson4/_js/global.js
+++ learning/lesson4/_js/global.js
@@ -1,15 +1,15 @@
 function renderTable() {
-	var source = $( "#entry-template" ).html();
+	var source   = $( "#entry-template" ).html();
 	var template = Handlebars.compile(source);
-	var context = { CATALOGUE_DATA };
-	var output = template(context);
+	var context  = { CATALOGUE_DATA };
+	var output   = template(context);
 	document.getElementById( "gems-table-body" ).innerHTML = output;
 };
 
 function renderCarousel() {
-	var source=$("#carousel-template").html();
+	var source   = $("#carousel-template").html();
 	var template = Handlebars.compile(source);
-	var context = { CATALOGUE_DATA };
-	var output = template(context);
+	var context  = { CATALOGUE_DATA };
+	var output   = template(context);
 	document.getElementById("gems-carousel").innerHTML = output;
 }
