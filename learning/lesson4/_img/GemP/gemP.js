8f9581c73ba97b079b8855a7f88fbe401fab5bcc
dicky <kidoblivious@gmail.com>
2017-05-12 11:23:26 +0100

    Lesson 4 final commit

---
+++ learning/lesson4/_img/GemP/gemP.js
@@ -0,0 +1,9 @@
+/**
+Data for gem P images.
+
+**/
+
+var GEMP_IMAGES_DATA = [
+	"20170419_124836.jpg",
+	"gemP_1.jpg"
+	];
--- learning/lesson4/_js/global.js
+++ learning/lesson4/_js/global.js
@@ -4,12 +4,19 @@ function renderTable() {
 	var context  = { CATALOGUE_DATA };
 	var output   = template(context);
 	document.getElementById( "gems-table-body" ).innerHTML = output;
-};
+}
 
 function renderCarousel() {
 	var source   = $("#carousel-template").html();
 	var template = Handlebars.compile(source);
 	var context  = { CATALOGUE_DATA };
 	var output   = template(context);
-	document.getElementById("gems-carousel").innerHTML = output;
+	document.getElementById( "gems-carousel" ).innerHTML = output;
 }
+
+function gemPage() {
+	var search = window.location.search;
+	var offset = search.lastIndexOf('=');
+	var id = search.substr(offset);
+	return id;
+}	
