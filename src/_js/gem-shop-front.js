9ff8638c7c12070ab6e242b6c0b20ddd1989ee80
dicky <kidoblivious@gmail.com>
2017-06-21 14:06:38 +0100

    main app updated to mongoose data, contact POST working presently.

--- src/_js/gem-shop-front.js
+++ src/_js/gem-shop-front.js
@@ -1,6 +1,42 @@
 var express = require( 'express' );
 var app = express();
 var bodyParser = require( 'body-parser' );
+var when = 0;
+var mongoose = require( 'mongoose' );
+var Contacts = require( '../../src/_js/data/schemas/contactsSchema' );
+
+var contactPost = function( request, response ) {
+	var document = { 
+		'emailAddress': request.body.emailAddress,
+		'emailSubject': request.body.emailSubject,
+		'emailBody': request.body.emailBody,
+		'mailingListConsent': false
+	};
+	var newContact = {};
+
+	mongoose.connect( 'mongodb://localhost/gem-shop-front' );
+
+	if ( request.body.mailingListConsent ) {
+		document.mailingListConsent = true;
+	}
+	console.log( request.body );
+	newContact = new Contacts( document );
+	newContact.save( function( err2 ) {
+		if ( err2 ) {
+			throw err2;
+		} else {
+		response.send( "POST request to the contact page<p>email add: " + document.emailAddress + 
+					"</p><p>email subject: " + document.emailSubject + 
+					"</p><p>email body: " + document.emailBody + 
+					"</p><p>mailing list consent " + document.mailingListConsent );
+		}
+	} );
+	mongoose.connection.close();
+};
+		
+
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( { extended: true } ) );
 
 /*	
 app.get( '/', function ( request, response ) {
@@ -14,20 +50,13 @@ app.post( '/', function ( request, response ) {
 } );
 */ 
 
-app.get( '/gem/:gemId', function ( request, response ) {
-	response.send( request.params.gemId );
-	console.log( "gemId: %s", request.params.gemId );
+app.get( '/gem/:_id', function ( request, response ) {
+	when = new Date();
+
+	response.send( request.params._id );
+	console.log( "%s _id: %s", when.toUTCString(), request.params._id );
 } );
 
-app.use( bodyParser.urlencoded( { extended: false } ) );
-app.use( bodyParser.json() );
-
-app.post( '/contact-us.js', function ( request, response ) {
-	var emailAddress  = request.params.emailAddress;
-	var emailSubject = request.params.emailSubject;
-	var emailBody = request.params.emailBody;
-
-	response.send( "POST request to the homepage<p>email add: " + emailAddress + "</p><p>email subject: " + emailSubject + "</p><p>email body: " + emailBody + "</p>" );
-} );
+app.post( '/contact-us.js', contactPost );
 
 module.exports = app;
