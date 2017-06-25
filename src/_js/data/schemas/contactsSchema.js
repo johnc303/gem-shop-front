1ca5ef65d8a9d7b4774bb89f67846aaac6e76930
dicky <kidoblivious@gmail.com>
2017-06-25 19:33:10 +0100

    Small corrections to schemas.

--- src/_js/data/schemas/contactsSchema.js
+++ src/_js/data/schemas/contactsSchema.js
@@ -1,10 +1,11 @@
 var mongoose = require( 'mongoose' );
 var Schema = mongoose.Schema;
-var contactsSchema = new Schema( {
-	emailAddress: String,
-	emailSubject: String,
-	emailBody: String,
-	mailingListConsent: Boolean
-} );
+var contactsSchema = new Schema( 
+	{
+		emailAddress: String,
+		emailSubject: String,
+		emailBody: String,
+		mailingListConsent: Boolean
+	} );
 var Contacts = mongoose.model( 'Contacts', contactsSchema );
 module.exports = Contacts;
--- src/_js/data/schemas/imagesSchema.js
+++ src/_js/data/schemas/imagesSchema.js
@@ -1,16 +1,15 @@
 // Schema for image name arrays
 var mongoose = require( 'mongoose' );
 var Schema = mongoose.Schema;
-var imagesSchema = new Schema( {
+var imagesSchema = new Schema(
 	{
 		gemId: 	{ 
 			type: 		Number,
-			ref:		'catalogue', 
+			ref:		'Catalogue', 
 			required: 	true,
 			unique: 	true
 		},
 		images:	[String]
-	}
-} );
+	} );
 var Images = mongoose.model( 'Images', imagesSchema );
 module.exports = Images;
