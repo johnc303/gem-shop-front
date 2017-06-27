95d6bb29ac6d6aba3b070e0b6f4edd9730be6a4d
dicky <kidoblivious@gmail.com>
2017-06-27 11:23:44 +0100

    grunt-prettier added.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,13 @@
 // Gruntfile.js
 
-module.exports = function( grunt ) {
-	grunt.initConfig( {
-		pkg: grunt.file.readJSON( 'package.json' )
-	} );
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json")
+  });
 
-	require( 'load-grunt-config' )( grunt );
+  require("load-grunt-config")(grunt);
 
-	grunt.loadTasks( 'src/_js/grunt/parseTree' );
-	grunt.loadNpmTasks( 'gruntify-eslint' );
+  grunt.loadTasks("src/_js/grunt/parseTree");
+  grunt.loadNpmTasks("gruntify-eslint");
+  grunt.loadNpmTasks("grunt-prettier");
 };
---
+++ grunt/prettier.js
@@ -0,0 +1,7 @@
+module.exports =  {
+	build: {
+		expand: true,
+		usetabs: true,
+		src: [ 'Gruntfile.js', 'src/**/*.js' ]
+	}	
+};
